3d69d87491c5e0bd17a616bdcb59a4e5f13b4010
Negrini085 <filippo.negrini@studenti.unimi.it>
2025-01-13 12:17:58 +0100

    Analisi della dimensione del blocco a variare del seed (ossia della traiettoria nello spazio delle fasi

--- Approfondimento/Ising1D/lBlk.sh
+++ Approfondimento/Ising1D/lBlk.sh
@@ -3,6 +3,8 @@
 input_file="param.in"       # Nome del file di input
 tempIsing=(0.5 1.0 1.5 2.0)                 # Temperature a cui simulo il modello
 sizeIsing=(1000 3000 6000 10000)            # Dimensioni del modello di Ising
+rgState=(0 10 20)                           # Seed random generator
+rgIncr=(5 15 25)                            # Incr random generator
 
 #--------------------------------------------------------------------------------#
 #       Ciclo sulla dimensione ed eseguo il programma a vari stati del pcg       #
@@ -15,10 +17,13 @@ for ((i=0; i<${#sizeIsing[@]}; i++)); do
     for t in "${tempIsing[@]}"; do
         sed -i "s/^temp\s\+.*/temp\t\t"$t"/" "$input_file"
 
-        # Eseguo programma e faccio analisi su lunghezza dei blocchi
-        ./Ising1D sim param.in analisi/lblk/lblk_t${t}_size${sizeIsing[i]}.out
-       	echo ""
-    	done
+        for ((j=2; j<=${#rgState[@]}+1; j++)); do 
+            sed -i "s/^state\s\+.*/state\t\t"${rgState[j-2]}"/" "$input_file"
+            sed -i "s/^incr\s\+.*/incr\t\t"${rgIncr[j-2]}"/" "$input_file"   
+    
+            # Eseguo programma e faccio analisi su lunghezza dei blocchi
+            ./Ising1D sim param.in analisi/lblk/sim/lblk_t${t}_size${sizeIsing[i]}_seed${j}.out
+        done
     
     done
 
