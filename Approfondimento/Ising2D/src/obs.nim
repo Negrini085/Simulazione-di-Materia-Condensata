d7d0f2ea3b0a3db8264f5a8635479e0f9140eca1
Negrini085 <filippo.negrini@studenti.unimi.it>
2025-01-22 16:58:13 +0100

    Aggiunta metodo per individuazione cluster

--- Approfondimento/Ising2D/src/obs.nim
+++ Approfondimento/Ising2D/src/obs.nim
@@ -1,3 +1,5 @@
+import std/sequtils
+
 proc calcolaEnergia*(modIsing: seq[seq[int]], nspin: int, acc: float32): float32 = 
     # Funzione per calcolare l'energia del modello di Ising
 
@@ -23,3 +25,57 @@ proc calcolaMagn*(modIsing: seq[seq[int]], nspin: int): float32 =
             magn += modIsing[i][j]
 
     return float32(magn)
+
+
+proc individuaClust*(modIsing: seq[seq[int]], nspin: int): seq[int] = 
+    # Funzione per calcolare la dimensione dei cluster (e distinguere i cluster)
+
+    var
+        appo: IsingCoord
+        startSpin: IsingCoord
+        stack: seq[IsingCoord]
+
+        upNeigh, downNeigh: IsingCoord
+        leftNeigh, rightNeigh: IsingCoord
+        
+        current_lab: int = 1
+        labels: seq[seq[int]] = newSeqWith(nspin, newSeqWith(nspin, 0))
+
+    # Leggo la matrice delle labels per poter individuare tutti i possibili cluster
+    for i in 0..<nspin:
+        for j in 0..<nspin:
+            
+            # Controllo se lo spin in analisi ha già ricevuto una label o meno
+            if labels[i][j] == 0:
+
+                # In questo caso devo iniziare a controllare quali spin appartengano allo stesso cluster di quello che stiamo 
+                # prendendo in considerazione. Per iniziare il ciclo aggiungo alla stack la coordinata che abbiamo individuato
+                # avere label nulla
+                stack.add(newCoord(i, j))
+
+
+                # Procediamo ora con la ricerca degli altri membri del cluster
+                while stack.len() > 0:
+                
+                    # Considero un elemento che fa parte della stack
+                    appo = stack.pop()
+
+                    # Aggiungo al cluster in base all'orientamento e in base alla label
+                    if modIsing[appo.xcoor][appo.ycoor] == modIsing[i][j] and labels[appo.xcoor][appo.ycoor] == 0:
+                    
+                        # Aggiorno la label del modello
+                        labels[appo.xcoor][appo.ycoor] = current_lab
+
+                        # Valuto i suoi primi vicini, da aggiungere alla stack
+                        upNeigh = newCoord(appo.xcoor, (appo.ycoor + 1) mod nspin)
+                        downNeigh = newCoord(appo.xcoor, (appo.ycoor + nspin - 1) mod nspin)
+                        leftNeigh = newCoord((appo.xcoor + 1) mod nspin, appo.ycoor)
+                        rightNeigh = newCoord((appo.xcoor + nspin - 1) mod nspin, appo.ycoor)
+
+                        # Aggiungo i primi vicini alla stack
+                        stack.add(upNeigh); stack.add(downNeigh); stack.add(leftNeigh); stack.add(rightNeigh)
+
+
+                # Una volta terminato il ciclo, e quindi l'individuazione del cluster, aggiorniamo il valore della label
+                current_lab += 1
+            
