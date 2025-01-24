c76acc336a61c11c9f9d268c03ffa2c9ba5e72a7
Negrini085 <filippo.negrini@studenti.unimi.it>
2025-01-24 08:57:15 +0100

    Filtrato per catena di spin lunga 1000

--- Approfondimento/Ising1D/analisi/magn0.0/obs/studioObs.sh
+++ Approfondimento/Ising1D/analisi/magn0.0/obs/studioObs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-input_file="param.in"                                            # Nome del file di input
-tempIsing=(1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0)     	      	 # Temperature a cui simulo il modello
-sizeIsing=(6000)   	                                     	 # Dimensioni del modello di Ising
+tempIsing=(1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0)      # Temperature a cui simulo il modello
+sizeIsing=(1000)   	                                     	 # Dimensioni del modello di Ising
 
 
 #---------------------------------------------------------------------#
@@ -11,18 +10,13 @@ sizeIsing=(6000)   	                                     	 # Dimensioni del mode
 
 # Ciclo per aggiornare la dimensione
 for ((i=0; i<${#sizeIsing[@]}; i++)); do
-    sed -i "s/^nspin\s\+.*/nspin\t\t"${sizeIsing[i]}"/" "$input_file"
-
     for t in "${tempIsing[@]}"; do
-        sed -i "s/^temp\s\+.*/temp\t\t"$t"/" "$input_file"
 
         # Eseguo programma per determinazione osservabili
-        ./Ising1D sim param.in analisi/magn0.0/obs/obs_lc_size${sizeIsing[i]}_t${t}.out
-    
-    done
+        rm obs_size${sizeIsing[i]}_t${t}.out
+        mv obs_lc_size${sizeIsing[i]}_t${t}.out obs_size${sizeIsing[i]}_t${t}.out
 
-    dim=${sizeIsing[i]}
-    echo "Eseguito studio termalizzazione per dimensione fissata $dim."    
+    done
 done
 
 echo "Studio della termalizzazione terminato."
